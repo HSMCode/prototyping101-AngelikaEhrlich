1dfeffeaed60805517bdd7673e86988701b1e9ef
Angelika Ehrlich <angelikaehrlich@gmx.net>
2020-11-05 17:06:27 +0100

    make car move
    script for simple car movement using WASD

---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed = .1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection * speed;
+    }
+}
