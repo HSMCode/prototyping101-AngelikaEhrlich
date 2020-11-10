4c7a7009bd81d205c2a54717d93bce1c1c3ba102
Angelika Ehrlich <angelikaehrlich@gmx.net>
2020-11-10 22:44:26 +0100

    make car move (again)
    change car movement (Input)

--- Assets/Scripts/playercontroller.cs
+++ Assets/Scripts/playercontroller.cs
@@ -1,18 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class playercontroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speed = .1f;
+    public float speed = 5f; // Speed for our hot red vehicle
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float xDirection = Input.GetAxis("Horizontal");
-        float zDirection = Input.GetAxis("Vertical");
+        // Move our hot red vehicle
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
 
-        transform.position += moveDirection * speed;
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
+
+        //output to log the position change
+        Debug.Log(transform.position);
     }
 }
