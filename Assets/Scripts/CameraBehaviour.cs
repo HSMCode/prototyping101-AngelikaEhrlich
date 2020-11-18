d1659637fcc93279645e2f28eb2b2da06211dc0b
Angelika Ehrlich <angelikaehrlich@gmx.net>
2020-11-18 14:58:51 +0100

    make camera follow car when rotating too
    camera now also follows the car when rotating

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -6,19 +6,26 @@ public class CameraBehaviour : MonoBehaviour
 {
     public GameObject car;
 
+    // variable to save world position transform of our car
+    private Transform target;
+
     // offset from local point of car to camera position
     private Vector3 offset = new Vector3(0f, 2f, -7f);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // set the car's transform to the variable target
+        target = car.transform;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        // set the camera to the position of the car with offset 
-        transform.position = car.transform.position + offset;
+        // set the camera to the position of the car with offset, converted to World Space
+        transform.position = target.TransformPoint(offset);
+
+        // Look at our car's transform position
+        transform.LookAt(target);
     }
 }
