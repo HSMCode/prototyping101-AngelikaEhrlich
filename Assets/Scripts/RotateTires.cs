a56a3f08f99cdf9559e66dd8f34adef3b626fad8
Angelika Ehrlich <angelikaehrlich@gmx.net>
2020-11-18 18:51:55 +0100

    make tires rotate
    Make the tires rotate, when the car is moving

---
+++ Assets/Scripts/RotateTires.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTires : MonoBehaviour
+{
+    private float rotationSpeed = 4;
+
+    // public GameObject wheels;
+    private float x = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("up"))
+        {
+        // If TRUE, e.g. if up  arrow key is pressed, do:
+        // Make the tires rotate
+            transform.Rotate(new Vector3(x, 0, 0) * rotationSpeed);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            // If TRUE, e.g. if down arrow key is pressed, do:
+            // Make the tires rotate
+            transform.Rotate(new Vector3(x, 0, 0) * rotationSpeed);
+        }
+    }
+}
